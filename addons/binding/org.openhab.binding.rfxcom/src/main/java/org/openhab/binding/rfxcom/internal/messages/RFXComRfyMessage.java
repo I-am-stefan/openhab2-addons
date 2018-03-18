1fd99b235109facc2ef57999a9f05a45038ff34d
I-am-stefan <23319818+I-am-stefan@users.noreply.github.com>
2018-03-18 20:27:23 +0100

    Update RFXComRfyMessage.java

--- addons/binding/org.openhab.binding.rfxcom/src/main/java/org/openhab/binding/rfxcom/internal/messages/RFXComRfyMessage.java
+++ addons/binding/org.openhab.binding.rfxcom/src/main/java/org/openhab/binding/rfxcom/internal/messages/RFXComRfyMessage.java
@@ -193,6 +193,22 @@ public class RFXComRfyMessage extends RFXComDeviceMessageImpl<RFXComRfyMessage.S
                     throw new RFXComUnsupportedChannelException("Can't convert " + type + " to Command");
                 }
                 break;
+                
+            case CHANNEL_VENETIAN_BLIND:
+                if (type instanceof OpenClosedType) {
+                    this.command = (type == OpenClosedType.CLOSED ? Commands.DOWN_SHORT : Commands.UP_SHORT);
+
+                } else if (type instanceof OnOffType) {
+                    this.command = (type == OnOffType.ON ? Commands.DOWN_SHORT : Commands.UP_SHORT);
+
+                } else if (type instanceof IncreaseDecreaseType) {
+                    this.command = (type == IncreaseDecreaseType.INCREASE ? Commands.DOWN_LONG : Commands.UP_LONG);
+
+                } else {
+                    throw new RFXComUnsupportedChannelException("Can't convert " + type + " to Command");
+                }
+                break;
+
 
             case CHANNEL_VENETIAN_BLIND_EU:
                 if (type instanceof OpenClosedType) {
